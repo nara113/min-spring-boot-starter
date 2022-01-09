530ea6564161a34a1560e5d1f13761d43cf27209
sedovila <0554my@naver.com>
2022-01-09 22:15:48 +0900

    Initial commit

---
+++ src/main/java/com/study/Holoman.java
@@ -0,0 +1,11 @@
+package com.study;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+public class Holoman {
+    String name;
+    int age;
+}
---
+++ src/main/java/com/study/HolomanConfiguration.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HolomanConfiguration {
+
+    @Bean
+    public Holoman holoman() {
+        return new Holoman("h", 1);
+    }
+}
